720ee8c71bf26e02c1ad29385a330f7935ceb0ef
Ali Kabalan <ali.kabalan.1994@gmail.com>
2021-05-09 14:23:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+class Cat{
+  constructor(name, sex){
+    this.name = name;
+    this.sex = sex;
+  }
+  speak(){
+    console.log(`${name} says meow!`);
+  }
+}
+
+class Dog{
+  constructor(name, sex){
+    this.name = name;
+    this.sex = sex;
+  }
+  speak(){
+    console.log(`${name} says Woof!`);
+  }
+}
+
+class Bird{
+  constructor(name, sex){
+    this.name = name;
+    this.sex = sex;
+  }
+  speak(){
+    if(this.name === "male"){
+    console.log(`"It's me, ${name}!`);
+  } else console.log(`${name} says squawk!`)
+  }
+}
